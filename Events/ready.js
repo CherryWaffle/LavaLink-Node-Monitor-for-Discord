1ec6d6b842fac71a1172603e23a0e09a469493c3
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-01-16 20:33:09 +0530

    Emojis Update

--- Events/ready.js
+++ Events/ready.js
@@ -32,18 +32,18 @@ module.exports = async (client) => {
 
                     `---------------------------- \n` +
                     `Hosting Server Info -> \n\n` +
-                    `Total Memory  :: ${Math.round(require('os').totalmem() / 1024 / 1024)} mb\n` +
-                    `Free Memory   :: ${Math.round(require('os').freemem() / 1024 / 1024)} mb\n` +
-                    `RSS           :: ${Math.round(process.memoryUsage().rss / 1024 / 1024)} mb\n` +
-                    `Heap Total    :: ${Math.round(process.memoryUsage().heapTotal / 1024 / 1024)} mb\n` +
-                    `Heap Used     :: ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)} mb\n` +
-                    `External      :: ${Math.round(process.memoryUsage().external / 1024 / 1024)} mb\n` +
-                    `Array Buffer  :: ${Math.round(process.memoryUsage().rss / 1024 / 1024)} mb\n` +
-                    `CPU Model     :: ${require('os').cpus()[0].model}\n` +
-                    `Cores         :: ${require('os').cpus().length}\n` +
-                    `Speed         :: ${require('os').cpus()[0].speed}Mhz\n` +
-                    `Platform      :: ${process.platform}\n` +
-                    `PID           :: ${process.pid}\n` +
+                    `📁 Total Memory  :: ${Math.round(require('os').totalmem() / 1024 / 1024)} mb\n` +
+                    `📁 Free Memory   :: ${Math.round(require('os').freemem() / 1024 / 1024)} mb\n` +
+                    `📁 RSS           :: ${Math.round(process.memoryUsage().rss / 1024 / 1024)} mb\n` +
+                    `📨 Heap Total    :: ${Math.round(process.memoryUsage().heapTotal / 1024 / 1024)} mb\n` +
+                    `📨 Heap Used     :: ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)} mb\n` +
+                    `📋 External      :: ${Math.round(process.memoryUsage().external / 1024 / 1024)} mb\n` +
+                    `📺 Array Buffer  :: ${Math.round(process.memoryUsage().rss / 1024 / 1024)} mb\n` +
+                    `🔧 CPU Model     :: ${require('os').cpus()[0].model}\n` +
+                    `🔃 Cores         :: ${require('os').cpus().length}\n` +
+                    `🥏 Speed         :: ${require('os').cpus()[0].speed}Mhz\n` +
+                    `💻 Platform      :: ${process.platform}\n` +
+                    `💻 PID           :: ${process.pid}\n` +
                     `\n` + `\`\`\``)
 					.addField("LavaLink Server" , `Click [here](https://github.com/freyacodes/Lavalink) for LavaLink's Source Code`)
                     .addField("LavaLink Monitor" , `Click [here](https://github.com/CherryWaffle/LavaLink-Node-Monitor-for-Discord) for LavaLink Monitor's Source Code`)
