c94326ed24b420167a8c0108c17af2a20c356547
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-01-16 20:09:48 +0530

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -15,7 +15,7 @@ module.exports = async (client) => {
             client.manager.nodes.forEach(node => {
               let info = []
               info.push(`Node: ${(node.options.identifier)}`)
-              info.push(`Status: ${node.connected ? "🟢 Online" : "🔴 Offline"}`)
+              info.push(`Status: ${node.connected ? "🟩 Online" : "🟥 Offline"}`)
               info.push(`Player: ${node.stats.players}`)
               info.push(`Playing Players: ${node.stats.playingPlayers}`)
               info.push(`Uptime: ${new Date(node.stats.uptime).toISOString().slice(11, 19)}`)
