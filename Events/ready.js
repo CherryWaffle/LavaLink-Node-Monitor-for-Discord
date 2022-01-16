89fd06fb62fee8b41079108560e0e13813b1b0f9
ranjithh-github <72537594+CherryWaffle@users.noreply.github.com>
2022-01-16 18:29:10 +0530

    Node Fixes

--- Events/ready.js
+++ Events/ready.js
@@ -21,6 +21,7 @@ module.exports = async (client) => {
               info.push(`Uptime: ${new Date(node.stats.uptime).toISOString().slice(11, 19)}`)
               info.push("\nCPU")
               info.push(`Cores: ${node.stats.cpu.cores}`)
+              info.push(`Memory: ${node.memory}`)
               info.push(`System Load: ${(Math.round(node.stats.cpu.systemLoad * 100) / 100).toFixed(2)}%`)
               info.push(`Lavalink Load: ${(Math.round(node.stats.cpu.lavalinkLoad * 100) / 100).toFixed(2)}%`)
               all.push(info.join('\n'))
